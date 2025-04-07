75a5d0e7d06d82421430bf67363c2fc8c045fa91
VladSH33 <vlad.shevkunov33@gmail.com>
2025-04-07 21:55:50 +0200

    initial

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,39 @@
+import webpack, { Configuration } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import { BuildOptions } from "./types/types";
+import path from "path";
+
+export function buildPlugins({
+  mode,
+  paths,
+}: BuildOptions): Configuration["plugins"] {
+  const isDev = mode === "development";
+  const isProd = mode === "production";
+
+  const plugins: Configuration["plugins"] = [
+    new HtmlWebpackPlugin({
+      template: paths.html,
+      favicon: path.resolve(paths.public, "favicon.ico"),
+    }),
+  ];
+
+  if (isDev) {
+    plugins.push(new webpack.ProgressPlugin());
+    plugins.push(new ForkTsCheckerWebpackPlugin());
+    plugins.push(new ReactRefreshWebpackPlugin());
+  }
+
+  if (isProd) {
+    plugins.push(
+      new MiniCssExtractPlugin({
+        filename: "css/[name].[contenthash:8].css",
+        chunkFilename: "css/[name].[contenthash:8].css",
+      })
+    );
+  }
+
+  return plugins;
+}
